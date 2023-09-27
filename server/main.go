5f4638d2cbbbfa61b0235fa99d42ca4e29c08195
motongxue <393888412@qq.com>
2023-09-27 10:31:37 +0800

    feat(server\main.go): 实现获取文件传输信息

--- server/main.go
+++ server/main.go
@@ -2,11 +2,17 @@ package server
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
+	"github.com/motongxue/concurrentChunkTransfer/models"
 	"log"
 	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
 )
 
 var (
@@ -60,8 +66,75 @@ func main() {
 
 // getFileTransferInfo 获取文件传输信息
 func getFileTransferInfo(ctx *gin.Context) {
-	// panic 方法未实现
-	panic("implement me")
+	var metaData models.FileMetaData
+	if err := ctx.ShouldBindJSON(&metaData); err != nil {
+		ctx.JSON(200, gin.H{
+			"code": 1,
+			"msg":  "参数错误",
+		})
+		return
+	}
+	log.Println("FileMetaData:", metaData)
+	// 将metaData写入redis
+	jsonMetaData, err := json.Marshal(metaData)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"code": 2,
+			"msg":  "序列化失败",
+		})
+		return
+	}
+	// 将FileMetaData写入redis
+	redisClient.SetEX(context.Background(), "FileMetaData:"+metaData.MD5, jsonMetaData, time.Hour)
+
+	md5 := metaData.MD5
+	info := models.FileTransferInfo{
+		MD5: md5,
+	}
+	// 从Redis中判断该dirName是否存在FileTransferInfo
+	if redisClient.Exists(context.Background(), "FileTransferInfo:"+md5).Val() == 0 {
+		// 不存在，创建
+		info.MD5 = metaData.MD5
+		info.Unreceived = make([]int, metaData.ChunkNum)
+		// 创建目录
+		if err := os.MkdirAll(filepath.Join(outputDir, metaData.MD5), os.ModeDir); err != nil {
+			log.Fatalln("Error creating output directory:", err)
+		}
+		for i := 0; i < metaData.ChunkNum; i++ {
+			info.Unreceived[i] = i
+		}
+		log.Println("Unreceived:", info.Unreceived)
+		// 以MD5为key的set
+		for _, num := range info.Unreceived {
+			// FileTransferInfo
+			_, err := redisClient.SAdd(context.Background(), "FileTransferInfo:"+md5, num).Result()
+			if err != nil {
+				log.Fatalln("Failed to add members to set:", err)
+				return
+			}
+		}
+	} else {
+		// FileTransferInfo存在，从Redis中获取
+		result, err := redisClient.MGet(context.Background(), "FileTransferInfo:"+md5).Result()
+		// 将result转换为info
+		for _, v := range result {
+			val, _ := strconv.Atoi(v.(string))
+			info.Unreceived = append(info.Unreceived, val)
+		}
+		if err != nil {
+			ctx.JSON(500, gin.H{
+				"code": 3,
+				"msg":  "获取文件信息失败",
+			})
+			return
+		}
+	}
+	ctx.JSON(200, gin.H{
+		"code": 0,
+		"msg":  "成功",
+		"data": info,
+	})
+	return
 }
 
 // ReceiveFile 接收文件
